f1670e05cf3c6e7684dde8a242a7bea71744e0b2
Jack DeMeyers <319723@wegmans.com>
2021-03-31 17:45:29 -0400

    use real api in web to check cors settings

--- src/Darty.Web/Program.cs
+++ src/Darty.Web/Program.cs
@@ -14,7 +14,7 @@ namespace Darty.Web
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient());
-            builder.Services.AddScoped(sp => new DartyApiClient("http://localhost:7071", sp.GetService<HttpClient>()));
+            builder.Services.AddScoped(sp => new DartyApiClient("https://darty-api-dev.azurewebsites.net", sp.GetService<HttpClient>()));
 
             await builder.Build().RunAsync();
         }

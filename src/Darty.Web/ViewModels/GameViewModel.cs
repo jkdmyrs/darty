a6aedc5e14bf6fb97094b6065a459c9aa7a499d4
Jack DeMeyers <319723@wegmans.com>
2021-04-05 18:43:04 -0400

    start scoreboard component

---
+++ src/Darty.Web/ViewModels/GameViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Darty.Web.ViewModels
+{
+    using Darty.Core.Resources.Responses;
+
+    public class GameViewModel
+    {
+        public GameViewModel(GameModelResponse model)
+        {
+            Player1 = new PlayerViewModel
+            {
+                IsWinner = model.Player1.IsWinner,
+                Name = model.Player1.Name,
+                Score = model.Player1.Score
+            };
+            Player2 = new PlayerViewModel
+            {
+                IsWinner = model.Player2.IsWinner,
+                Name = model.Player2.Name,
+                Score = model.Player2.Score
+            };
+            HasWinner = model.HasWinner;
+        }
+
+        public PlayerViewModel Player1 { get; set; }
+        public PlayerViewModel Player2 { get; set; }
+        public bool HasWinner { get; set; }
+    }
+}
---
+++ src/Darty.Web/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Darty.Core.Resources.Responses;
+using System.Collections.Generic;
+
+namespace Darty.Web.ViewModels
+{
+    public class PlayerViewModel
+    {
+        private List<DartThrowResponse> _dartThrows;
+
+        public string Name { get; set; }
+        public bool IsWinner { get; set; }
+        public int Score { get; set; }
+    }
+}

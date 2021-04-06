a773607ed3e43ea755464c688a4e3d1d59daa3f3
Jack DeMeyers <319723@wegmans.com>
2021-04-06 10:57:13 -0400

    update Darty.iOS to xCode project instead of Xamarin

---
+++ src/Darty.iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Darty
+//
+//  Created by Jackson DeMeyers on 4/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ src/Darty.iOS/DartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartyApp.swift
+//  Darty
+//
+//  Created by Jackson DeMeyers on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct DartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
